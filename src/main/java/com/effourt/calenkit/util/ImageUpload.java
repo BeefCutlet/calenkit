d0ec8004dbe400174a2647c6c3478a13ef5d7198
Owner <whysabi492@naver.com>
2023-06-15 16:28:22 +0900

    [STYLE] 주석 제거 및 이미지 파일 저장 경로 변수화

--- src/main/java/com/effourt/calenkit/util/ImageUpload.java
+++ src/main/java/com/effourt/calenkit/util/ImageUpload.java
@@ -1,6 +1,7 @@
 package com.effourt.calenkit.util;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,17 +17,17 @@ import java.util.UUID;
 @Service
 public class ImageUpload {
 
+    @Value("${file.route}")
+    private String filePath;
     private final MessageSource ms;
 
     public String uploadImage(MultipartFile file) throws IOException {
         byte[] bytes = file.getBytes();
         String originalFilename = file.getOriginalFilename();
         //고유 ID값을 부여해서 이미지 이름 중복되지 않게 처리.
-//        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
         String filename = UUID.randomUUID().toString() + "_" + originalFilename;
         //이미지가 저장될 경로
-//        String savePath = "C:/Users/이진규/IdeaProjects/calenkit6/src/main/resources/static/img/";
-        String savePath = ms.getMessage("file.save-route", null, null);
+        String savePath = ms.getMessage(filePath, null, null);
 
         Path path = Paths.get(savePath + filename);
         //파일 저장
