dc28877991b7f6b7ee76130bf3cb8050a27b1b3a
BeefCutlet <whysabi492@naver.com>
2023-10-20 09:39:31 +0900

    파일 이름 형식 수정

--- src/main/java/com/effourt/calenkit/util/ImageUpload.java
+++ src/main/java/com/effourt/calenkit/util/ImageUpload.java
@@ -1,8 +1,6 @@
 package com.effourt.calenkit.util;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 
@@ -27,9 +25,18 @@ public class ImageUpload {
     private String bucket;
     private final AmazonS3Client amazonS3Client;
 
+    /**
+     * 이미지 업로드 메서드
+     * Amazon S3의 bucket에 파일 저장
+     * 파일 이름 앞에는 UUID 8자리를 붙이고, 언더바로 구분
+     * @param multipartFile
+     * @return
+     * @throws IOException
+     */
     public String uploadImage(MultipartFile multipartFile) throws IOException {
         //고유 ID값을 부여해서 이미지 이름 중복되지 않게 처리.
-        String filename = UUID.randomUUID().toString() + "_" + URLEncoder.encode(multipartFile.getOriginalFilename());
+        String filename = UUID.randomUUID().toString().substring(0, 8) + "_"
+                + multipartFile.getOriginalFilename();
         String filePath = OBJECT_PATH + "/" + filename;
         log.info("filename=" + filename);
         log.info("filePath=" + filePath);
