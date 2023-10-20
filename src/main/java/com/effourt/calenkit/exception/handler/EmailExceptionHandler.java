f6fdab74207f004f6c7ececa0b10f0b9aab7f8da
BeefCutlet <whysabi492@naver.com>
2023-10-20 09:39:59 +0900

    이메일 전송 실패 시 예외 처리 추가

--- src/main/java/com/effourt/calenkit/exception/handler/EmailExceptionHandler.java
+++ src/main/java/com/effourt/calenkit/exception/handler/EmailExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.mail.MailSendException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.mail.AuthenticationFailedException;
 import javax.mail.internet.AddressException;
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,13 +15,19 @@ public class EmailExceptionHandler {
 
     @ExceptionHandler(AddressException.class)
     public String addressEx(Exception e, HttpServletRequest request) {
-        log.error("[EmailSendingError] Exception occurred at {}, ExceptionMessage={}, ", request.getRequestURI(), e.getMessage());
+        log.error("[EmailSendingError] AddressException occurred at {}, ExceptionMessage={}, ", request.getRequestURI(), e.getMessage());
         return "이메일 전송에 실패하였습니다.";
     }
 
     @ExceptionHandler(MailSendException.class)
     public String mailSendEx(Exception e, HttpServletRequest request) {
-        log.error("[EmailSendingError] Exception occurred at {}, ExceptionMessage={}, ", request.getRequestURI(), e.getMessage());
+        log.error("[EmailSendingError] MailSendException occurred at {}, ExceptionMessage={}, ", request.getRequestURI(), e.getMessage());
+        return "유효하지 않은 이메일입니다.";
+    }
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public String authenticationFailedException(Exception e, HttpServletRequest request) {
+        log.error("[EmailSendingError] AuthenticationFailedException occurred at {}, ExceptionMessage={}, ", request.getRequestURI(), e.getMessage());
         return "유효하지 않은 이메일입니다.";
     }
 }
