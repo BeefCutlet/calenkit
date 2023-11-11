ce8451810aebbe06fb21217095bd4e8c1d7c108a
OSU <whysabi492@naver.com>
2023-11-12 01:36:53 +0900

    S3 관련 에러 발생 방지

--- src/main/java/com/effourt/calenkit/CalenkitApplication.java
+++ src/main/java/com/effourt/calenkit/CalenkitApplication.java
@@ -2,9 +2,18 @@ package com.effourt.calenkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				ContextInstanceDataAutoConfiguration.class,
+				ContextStackAutoConfiguration.class,
+				ContextRegionProviderAutoConfiguration.class
+		}
+)
 @EnableFeignClients(basePackages = {"com.effourt.calenkit.client"})
 public class CalenkitApplication {
 
