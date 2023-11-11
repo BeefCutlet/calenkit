b79d88dcc7d6d98e72a2cacc83cbe702ef25c374
OSU <whysabi492@naver.com>
2023-11-12 01:37:45 +0900

    Spring Security 설정을 최신 버전으로 변경

--- src/main/java/com/effourt/calenkit/config/SpringSecurityConfig.java
+++ src/main/java/com/effourt/calenkit/config/SpringSecurityConfig.java
@@ -3,20 +3,19 @@ package com.effourt.calenkit.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
-        http.csrf().disable();//csrf 방지 기능 비활성화 -- 테스트용 - 배포 시에 삭제할 것
-        http.formLogin().disable();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable();
+        return http.build();
     }
 
     @Bean
