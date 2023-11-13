04d384b156f4f0f1786641bf3a868c4ce4c13cd0
OSU <whysabi492@naver.com>
2023-11-14 03:05:02 +0900

    S3 관련 코드 제거

--- src/main/java/com/effourt/calenkit/CalenkitApplication.java
+++ src/main/java/com/effourt/calenkit/CalenkitApplication.java
@@ -2,18 +2,9 @@ package com.effourt.calenkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
-import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
-import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(
-		exclude = {
-				ContextInstanceDataAutoConfiguration.class,
-				ContextStackAutoConfiguration.class,
-				ContextRegionProviderAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 @EnableFeignClients(basePackages = {"com.effourt.calenkit.client"})
 public class CalenkitApplication {
 
