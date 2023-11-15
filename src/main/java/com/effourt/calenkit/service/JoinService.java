c04f4d4b958063a6fa285a566264affda29dc6e1
OSU <whysabi492@naver.com>
2023-11-16 03:58:25 +0900

    OAuth Client 적용으로 인해 불필요해진 코드 제거

--- src/main/java/com/effourt/calenkit/service/JoinService.java
+++ src/main/java/com/effourt/calenkit/service/JoinService.java
@@ -1,7 +1,6 @@
 package com.effourt.calenkit.service;
 
 import com.effourt.calenkit.domain.Member;
-import com.effourt.calenkit.dto.AuthUserInfoResponse;
 import com.effourt.calenkit.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,21 +19,5 @@ public class JoinService {
     @Transactional
     public void joinByEmail(Member member) {
         memberRepository.save(member);
-    }
-
-    /**
-     * 소셜로그인 시 회원가입
-     * 액세스 토큰, 리프레시 토큰 저장 및 회원 정보 저장
-     * @param authUserInfoResponse 저장할 회원 정보
-     * @return 저장된 회원 정보
-     */
-    @Transactional
-    public Member joinBySns(AuthUserInfoResponse authUserInfoResponse) {
-        Member member = new Member();
-        member.setMemId(authUserInfoResponse.getEmail());
-        member.setMemName(authUserInfoResponse.getNickname());
-        member.setMemImage(authUserInfoResponse.getProfileImage());
-        memberRepository.save(member);
-        return member;
     }
 }
