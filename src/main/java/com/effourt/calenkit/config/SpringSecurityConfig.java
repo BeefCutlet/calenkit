6b04f2d1e5ad9c577561404b2c8dc5aa506b804c
OSU <whysabi492@naver.com>
2023-11-16 03:54:08 +0900

    OAuth Client를 이용한 카카오 소셜 로그인 설정 추가

--- src/main/java/com/effourt/calenkit/config/SpringSecurityConfig.java
+++ src/main/java/com/effourt/calenkit/config/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.effourt.calenkit.config;
 
+import com.effourt.calenkit.security.Oauth2CustomUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,13 +10,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SpringSecurityConfig {
+
+    private final Oauth2CustomUserService oauth2CustomUserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .formLogin().disable();
+        http.oauth2Login()
+                .defaultSuccessUrl("/login/return-uri")
+                .userInfoEndpoint()
+                .userService(oauth2CustomUserService);
         return http.build();
     }
 
